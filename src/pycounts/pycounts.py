32237713fb1d08e1c532f9586c1bac1d75a7e857
Lilmillyrock <lilliantm7@gmail.com>
2024-04-05 15:48:39 -0700

    docs: added robust docstrings to functions

--- src/pycounts/pycounts.py
+++ src/pycounts/pycounts.py
@@ -4,6 +4,20 @@ from string import punctuation
 def load_text(input_file):
     """
     Load text from a text file and return as a string.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to text file.
+
+    Returns
+    -------
+    str
+        Text file contents.
+
+    Examples
+    --------
+    >>> load_text("text.txt")
     """
     with open(input_file, "r") as file:
         text = file.read()
@@ -12,6 +26,21 @@ def load_text(input_file):
 def clean_text(text):
     """
     Lowercase and remove punctuation from a string.
+
+    Parameters
+    ----------
+    text : str
+        Text to clean.
+
+    Returns
+    -------
+    str
+        Cleaned text.
+
+    Examples
+    --------
+    >>> clean_text("Early optimization is the root of all evil!")
+    'early optimization is the root of all evil'
     """
     text = text.lower()
     for p in punctuation:
@@ -20,7 +49,24 @@ def clean_text(text):
 
 def count_words(input_file):
     """
-    Count unique words in a string.
+    Count unique words in a text file.
+
+    Words are made lowercase and punctuation is removed 
+    before counting.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to text file.
+
+    Returns
+    -------
+    collections.Counter
+        dict-like object where keys are words and values are counts.
+
+    Examples
+    --------
+    >>> count_words("text.txt")
     """
     text = load_text(input_file)
     text = clean_text(text)
